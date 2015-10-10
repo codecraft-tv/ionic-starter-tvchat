c833a6d768b96d40f9ee42c97273ac24ebcb3e50
Asim Hussain <jawache@gmail.com>
2015-10-10 21:32:25 +0100

    Added in code for load user

--- js/services/user.js
+++ js/services/user.js
@@ -57,15 +57,36 @@ app.service('UserService', function (FIREBASE_URL,
 		loadUser: function () {
 			var d = $q.defer();
 
-			//TODO
+			// UNCOMMENT WHEN GOING THROUGH LECTURES
+			/*
+			// Check local storage to see if there is a user already logged in
+			var currentUserId = $localstorage.get('tvchat-user', null);
+			if (currentUserId && currentUserId != "null") {
+				// If there is a logged in user then load up the object via firebase
+				// and use $firebaseObject to keep it in sync between our
+				// application and firebase.
+				console.debug("Found previously logged in user, loading from firebase ", currentUserId);
+				var user = $firebaseObject(usersRef.child(currentUserId));
+				user.$loaded(function () {
+					// When we are sure the object has been completely
+					// loaded from firebase then resolve the promise.
+					self.current = user;
+					self.identifyUser();
+					d.resolve(self.current);
+				});
+			} else {
+				d.resolve();
+			}
 
+			*/
 			return d.promise;
 		},
 		/*
 		 Logout the user
 		 */
 		logoutUser: function () {
-			//TODO
+			$localstorage.set('tvchat-user', null);
+			self.current = {};
 		},
 		/*
 		 Login the user
@@ -73,119 +94,129 @@ app.service('UserService', function (FIREBASE_URL,
 		loginUser: function () {
 			var d = $q.defer();
 
-			//
-			// Initiate the facebook login process
-			//
-			console.log('Calling facebook login');
-			openFB.login(
-				function (response) {
-					console.log(response);
-					if (response.status === 'connected') {
-						console.log('Facebook login succeeded');
-						//
-						// Facebook login was a success, get details about the current
-						// user
-						//
-						var token = response.authResponse.accessToken;
-						openFB.api({
-							path: '/me',
-							params: {},
-							success: function (userData) {
-								console.log('Got data from facebook about current user');
-								console.log(userData);
-								//
-								// We got details of the current user now authenticate via firebase
-								//
-								console.log('Authenticating with firebase');
+			self.loadUser().then(function (user) {
+				if (user) {
+					d.resolve(self.current);
+				}
+				else {
 
-								/* UNCOMMENT WHEN GOING THROUGH LECTURES
-								var auth = $firebaseAuth(ref);
-								auth.$authWithOAuthToken("facebook", token)
-									.then(function (authData) {
-										console.log("Authentication success, logged in as:", authData.uid);
-										console.log(authData);
+					//
+					// Initiate the facebook login process
+					//
+					console.log('Calling facebook login');
+					openFB.login(
+						function (response) {
+							console.log(response);
+							if (response.status === 'connected') {
+								console.log('Facebook login succeeded');
+								//
+								// Facebook login was a success, get details about the current
+								// user
+								//
+								var token = response.authResponse.accessToken;
+								openFB.api({
+									path: '/me',
+									params: {},
+									success: function (userData) {
+										console.log('Got data from facebook about current user');
+										console.log(userData);
 										//
-										// We've authenticated, now it's time to either get an existing user
-										// object or create a new one.
+										// We got details of the current user now authenticate via firebase
 										//
-										usersRef.child(authData.uid)
-											.transaction(function (currentUserData) {
-												if (currentUserData === null) {
-													//
-													// If the transaction is a success and the current user data is
-													// null then this is the first time firebase has seen this user id
-													// so this user is NEW.
-													//
-													// Any object we return from here will be used as the user data
-													// in firebase
-													//
-													return {
-														'name': userData.name,
-														'profilePic': 'http://graph.facebook.com/' + userData.id + '/picture',
-														'userId': userData.id
-													};
-												}
-											},
-											function (error, committed) {
+										console.log('Authenticating with firebase');
+
+										// UNCOMMENT WHEN GOING THROUGH LECTURES
+										/*
+										var auth = $firebaseAuth(ref);
+										auth.$authWithOAuthToken("facebook", token)
+											.then(function (authData) {
+												console.log("Authentication success, logged in as:", authData.uid);
+												console.log(authData);
 												//
-												// This second function in the transaction clause is always called
-												// whether the user was created or is being retrieved.
+												// We've authenticated, now it's time to either get an existing user
+												// object or create a new one.
 												//
-												// We want to store the userid in localstorage as well as load the user
-												// and store it in the self.current property.
+												usersRef.child(authData.uid)
+													.transaction(function (currentUserData) {
+														if (currentUserData === null) {
+															//
+															// If the transaction is a success and the current user data is
+															// null then this is the first time firebase has seen this user id
+															// so this user is NEW.
+															//
+															// Any object we return from here will be used as the user data
+															// in firebase
+															//
+															return {
+																'name': userData.name,
+																'profilePic': 'http://graph.facebook.com/' + userData.id + '/picture',
+																'userId': userData.id
+															};
+														}
+													},
+													function (error, committed) {
+														//
+														// This second function in the transaction clause is always called
+														// whether the user was created or is being retrieved.
+														//
+														// We want to store the userid in localstorage as well as load the user
+														// and store it in the self.current property.
+														//
+														$localstorage.set('tvchat-user', authData.uid);
+														self.current = $firebaseObject(usersRef.child(authData.uid));
+														self.current.$loaded(function () {
+															// When we are sure the object has been completely
+															// loaded from firebase then resolve the promise.
+															d.resolve(self.current);
+														});
+													});
+											})
+											.catch(function (error) {
+												console.error("Authentication failed:", error);
 												//
-												$localstorage.set('tvchat-user', authData.uid);
-												self.current = $firebaseObject(usersRef.child(authData.uid));
-												self.current.$loaded(function () {
-													// When we are sure the object has been completely
-													// loaded from firebase then resolve the promise.
-													d.resolve(self.current);
+												// We've failed to authenticate, show the user an error message.
+												//
+												$ionicPopup.alert({
+													title: "Error",
+													template: 'There was an error logging you in with facebook, please try later.'
 												});
+												d.reject(error);
 											});
-									})
-									.catch(function (error) {
-										console.error("Authentication failed:", error);
+											*/
+
+
+									},
+									error: function (error) {
+										console.error('Facebook error: ' + error.error_description);
 										//
-										// We've failed to authenticate, show the user an error message.
+										// There was an error calling the facebook api to get details about the
+										// current user. Show the user an error message
 										//
 										$ionicPopup.alert({
-											title: "Error",
-											template: 'There was an error logging you in with facebook, please try later.'
+											title: "Facebook Error",
+											template: error.error_description
 										});
 										d.reject(error);
-									});
-								*/
-
-							},
-							error: function (error) {
-								console.error('Facebook error: ' + error.error_description);
+									}
+								});
+							} else {
+								console.error('Facebook login failed');
 								//
-								// There was an error calling the facebook api to get details about the
-								// current user. Show the user an error message
+								// There was an error authenticating with facebook
+								// Show the user an error message
 								//
 								$ionicPopup.alert({
 									title: "Facebook Error",
-									template: error.error_description
+									template: 'Failed to login with facebook'
 								});
 								d.reject(error);
 							}
+						},
+						{
+							scope: 'email' // Comma separated list of permissions to request from facebook
 						});
-					} else {
-						console.error('Facebook login failed');
-						//
-						// There was an error authenticating with facebook
-						// Show the user an error message
-						//
-						$ionicPopup.alert({
-							title: "Facebook Error",
-							template: 'Failed to login with facebook'
-						});
-						d.reject(error);
-					}
-				},
-				{
-					scope: 'email' // Comma separated list of permissions to request from facebook
-				});
+				}
+			});
 			return d.promise;
 		}
 	};
